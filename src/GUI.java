6d9197efad4698a4a11ee260383dd0f309602a49
alex <alex.garrison.49@gmail.com>
2023-08-07 18:02:30 +0100

    added option panel, and button click functions

--- src/GUI.java
+++ src/GUI.java
@@ -1,12 +1,19 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GUI extends JFrame {
     public static GUI frame;
 
     private JPanel mainPanel;
+    private JPanel optionPanel;
     private JPanel[] boardPanels;
+    private JButton newGameButton;
+    private JComboBox<String> selectMode;
+    private JLabel bottomLabel;
     private JButton[][][] cells;
 
     public GUI() {
@@ -31,8 +38,11 @@ public class GUI extends JFrame {
             mainPanel.add(boardPanel);
         }
 
+        optionPanel = createOptionPanel();
+
         setLayout(new BorderLayout());
         add(mainPanel, BorderLayout.CENTER);
+        add(optionPanel, BorderLayout.SOUTH);
     }
 
     private JPanel createBoardPanel(int boardIndex) {
@@ -43,7 +53,8 @@ public class GUI extends JFrame {
 
         for (int row = 0; row < 3; row++) {
             for (int col = 0; col < 3; col++) {
-                JButton cell = new JButton();
+                JButton cell = new JButton("X");
+                cell.addActionListener(new CellClickListener(boardIndex, row, col));
                 cell.setPreferredSize(new Dimension(50,50));
                 cell.setBorder(new LineBorder(Color.BLACK,1));
                 cell.setOpaque(false);
@@ -57,7 +68,66 @@ public class GUI extends JFrame {
         return boardPanel;
     }
 
+    private JPanel createOptionPanel() {
+        JPanel newOptionPanel = new JPanel();
+        newOptionPanel.setLayout(new BoxLayout(newOptionPanel, BoxLayout.X_AXIS));
+        newOptionPanel.setBorder(new EmptyBorder(0,5,5,5));
+
+        newGameButton = new JButton("New Game");
+        newGameButton.setFont(new Font("Arial", Font.PLAIN, 15));
+        newGameButton.addActionListener(new NewGameClickListener());
+
+        selectMode = new JComboBox<>(new String[]{"PvP", "PvAI", "AIvAI"});
+        selectMode.setFont(new Font("Arial", Font.PLAIN, 15));
+        selectMode.addActionListener(new SelectModeListener());
+
+        bottomLabel = new JLabel("-");
+        bottomLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+        bottomLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        newOptionPanel.add(newGameButton);
+        newOptionPanel.add(Box.createRigidArea(new Dimension(5,-1)));
+        newOptionPanel.add(selectMode);
+        newOptionPanel.add(Box.createHorizontalGlue());
+        newOptionPanel.add(bottomLabel);
+        newOptionPanel.add(Box.createHorizontalGlue());
+
+        return newOptionPanel;
+    }
+
+    private class CellClickListener implements ActionListener {
+        private final int boardIndex;
+        private final int row;
+        private final int col;
+
+        public CellClickListener(int boardIndex, int row, int col) {
+            this.boardIndex = boardIndex;
+            this.row = row;
+            this.col = col;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JButton cell = (JButton) e.getSource();
+        }
+    }
+
+    private class NewGameClickListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JButton newGame = (JButton) e.getSource();
+        }
+    }
+
+    private class SelectModeListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JComboBox selectMode = (JComboBox) e.getSource();
+        }
+    }
+
     public static void main(String[] args) {
+        System.setProperty( "apple.awt.application.appearance", "system" );
 
         SwingUtilities.invokeLater(() -> {
             frame = new GUI();
